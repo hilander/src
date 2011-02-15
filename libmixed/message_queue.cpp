858fd453b01101c329a21dbe304b2f34390af5b7
Mariusz Barycz <mariusz.barycz@gmail.com>
2011-02-15 01:00:18 +0100

    Refactoring: 2: adding 'nonblocking' message queues.

--- libmixed/message_queue.cpp
+++ libmixed/message_queue.cpp
@@ -1,6 +1,7 @@
 #include "message_queue.hpp"
 #include "scheduler_tools.hpp"
 #include <list>
+#include <errno.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // message                                                                     /
@@ -24,39 +25,83 @@ scheduler::message::used()
   return _used;
 }
 
+void
+scheduler::message::set_used()
+{
+  _used = true;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // message_queue                                                               /
 ////////////////////////////////////////////////////////////////////////////////
+// private                                                                     /
+////////////////////////////////////////////////////////////////////////////////
+int
+scheduler::message_queue::get_readable_queue()
+{
+	for (int i = 0; i < queue_count; i++ )
+	{
+		if ( ums[i] == READABLE )
+		{
+			if ( mls[i].empty() == false )
+			{
+				return i;
+			}
+			else
+			{
+				ums[i] = ZEROED;
+			}
+		}
+	}
+	return EAGAIN;
+}
+
+int
+scheduler::message_queue::get_writeable_queue()
+{
+	for (int i = 0; i < queue_count; i++ )
+	{
+		if ( ( ums[i] == ZEROED ) && ( ums[ (i+1)%2 ] == WRITEABLE ) )
+		{
+			ums[    i    ] = WRITEABLE;
+			ums[ (i+1)%2 ] = READABLE;
+			return i;
+		}
+	}
+	return EAGAIN;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// message_queue                                                               /
+////////////////////////////////////////////////////////////////////////////////
+// public                                                                      /
+////////////////////////////////////////////////////////////////////////////////
 scheduler::message_queue::message_queue()
 {
+	ums[0] = ums[1] = ZEROED;
 }
 
 scheduler::message_queue::~message_queue()
 {
 }
 
-void
-scheduler::message_queue::clean()
+bool
+scheduler::message_queue::read( message* m )
 {
-  message candidate;
-  std::list< message >::iterator erase_it = ml.end();
+	messages_t& ml = get_readable_queue();
 
-  for ( std::list< message >::iterator it = ml.begin();
+  for ( messages_t::iterator it = ml.begin();
       it != ml.end();
       it++
       )
   {
-    if ( erase_it != ml.end() )
+    message* tm = *it;
+    if ( tm->used() == false )
     {
-      ml.erase( erase_it );
-      erase_it = ml.end();
-    }
-
-    message m = *it;
-    if ( m.used() )
-    {
-      erase_it = it;
+      m = tm;
+      tm->set_used();
+      return true;
     }
   }
+  return false;
 }
-
